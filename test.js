c9c8d048d1e409d6603a2c745604135ad65ecefc
chan <chankang17@gmail.com>
2023-03-08 02:15:27 -0500

    working - just need to make the templating work correctly

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 const mocha = require('mocha')
 
 const testRunner = new mocha({})
-const path = process.argv.slice(2)
-console.log("path: ", path)
-testRunner.addFile(process.argv.slice(2))
+// const path = process.argv.slice(2)
+const path = "test_one.js"
+testRunner.addFile(path)
 testRunner.run(function (err) {
     err && console.error(err.stack || err);
     exit(err ? 1 : 0);
